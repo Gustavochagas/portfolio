ff3d726a73548820ef5da82e71f0945e54bbcb4e
gustavochagass <gustavochagasb@gmail.com>
2020-05-08 21:05:51 -0300

    Updates

---
+++ precache-manifest.376652f79ff420d7fe5e27680c62bf52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a14f03320b85f4dfe28f68fd2e8784",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "958bef6e7dfd24536f67",
+    "url": "/portfolio/static/css/main.2ebeda58.chunk.css"
+  },
+  {
+    "revision": "4a387d69afce3cbbdc7a",
+    "url": "/portfolio/static/js/2.afba6113.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.afba6113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958bef6e7dfd24536f67",
+    "url": "/portfolio/static/js/main.2e4af29b.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "8bf2de472eb4b61c2ac60349298cf493",
+    "url": "/portfolio/static/media/illustration.8bf2de47.svg"
+  }
+]);
